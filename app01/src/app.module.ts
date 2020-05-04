c2604608979c72afce20213bc1d53ea5ae38a8c1
Mkozi <mkozi.junk@gmail.com>
2020-05-03 22:06:09 -0500

    Middleware - end

--- app01/src/app.module.ts
+++ app01/src/app.module.ts
@@ -1,11 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
+
+function logger(req: Request, res: Response, next: Function) {
+  console.log('Functional Middleware Logger: request...');
+}
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      // .apply(cors(), helmet(), logger)
+      // .forRoutes(CatsController);
+
+    consumer
+      .apply(logger)
+      .forRoutes(CatsController);
+
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .exclude({ path: 'cats', method: RequestMethod.POST })
+    //   .forRoutes(CatsController);
+    //   // .forRoutes({
+    //   //   path: 'cats',
+    //   //   method: RequestMethod.GET
+    //   // });
+
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes('cats');
+  }
+}
